17ea3504df8448ede40659f2b8d007eab839b2fc
Fernando Ribeiro <pinguim.ribeiro@gmail.com>
2015-08-22 23:37:06 +0100

    add spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Spanish language file for lsb plugin
+ *
+ * @author Fernando Ribeiro <pinguim.ribeiro@gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+$lang['twitter_text']    = 'Twitter';
+$lang['facebook_text']   = 'Facebook';
+$lang['googleplus_text'] = 'Google+';
+$lang['linkedin_text']   = 'LinkedIn';
+$lang['pinterest_text']  = 'Pinterest';
+$lang['tumblr_text']     = 'Tumblr';
+$lang['reddit_text']     = 'Reddit';
---
+++ lang/es/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spanish language file for lsb plugin
+ *
+ * @author Fernando Ribeiro <pinguim.ribeiro@gmail.com>
+ */
+
+// keys need to match the config setting name
+// $lang['fixme'] = 'FIXME';
+
+$lang['networks']   = 'Lista de tus butones sociales favoritos, separados por espacio y en orden de aparición.';
+$lang['networks']  .= ' Son nombre válidos: twitter facebook googleplus linkedin pinterest tumblr reddit';
+
+$lang['display']    = 'Opciones de visualización: muestra texto o solamente el icono.';
