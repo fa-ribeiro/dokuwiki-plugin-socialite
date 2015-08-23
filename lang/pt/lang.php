53497c76b5c8c2d98c53226bfdb41d4743a9a6e0
Fernando Ribeiro <pinguim.ribeiro@gmail.com>
2015-08-23 19:33:24 +0100

    social network names are now hardcoded

--- lang/pt/lang.php
+++ lang/pt/lang.php
@@ -10,11 +10,3 @@
 
 // custom language strings for the plugin
 // $lang['fixme'] = 'FIXME';
-
-$lang['twitter_text']    = 'Twitter';
-$lang['facebook_text']   = 'Facebook';
-$lang['googleplus_text'] = 'Google+';
-$lang['linkedin_text']   = 'LinkedIn';
-$lang['pinterest_text']  = 'Pinterest';
-$lang['tumblr_text']     = 'Tumblr';
-$lang['reddit_text']     = 'Reddit';
