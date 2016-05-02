f0ebaaf62228241fade7b7cdc3cbbc138828f961
Makito Katsube <katsubemakito@gmail.com>
2016-05-02 19:47:02 +0000

    add the Japanese on admin.

---
+++ lang/ja/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Japanese language file for lsb plugin
+ *
+ * @author Makito Katsube <katsubemakito@gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
