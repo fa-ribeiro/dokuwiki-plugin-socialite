b867346cc54b5df3df6e0bef68f7cb0a7ab0189e
Michel <mjbohn@gmail.com>
2017-12-13 09:44:38 +0100

    Add german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * German language file for socialite plugin
+ *
+ * @author Michael Bohn <mjbohn@gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Ihr Men&uuml;eintrag';
+
