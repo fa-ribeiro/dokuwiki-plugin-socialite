1330c0245556f64da03a803db83c0c2706598cfc
Gerrit Uitslag <klapinklapin@gmail.com>
2018-04-23 22:46:56 +0200

    no string concatenation fr

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -5,11 +5,7 @@
  * @author Fabrice DEJAIGHER <fabrice@chtiland.com>
  */
 
-// keys need to match the config setting name
-// $lang['fixme'] = 'FIXME';
-
-$lang['networks']           = 'Liste des boutons de vos réseaux sociaux préférés, séparés par un espace et dans l\'ordre d\'affichage. Noms acceptés : ';
-$lang['networks']          .= 'twitter facebook googleplus linkedin pinterest tumblr reddit taringa stumbleupon telegram hackernews xing vk email';
+$lang['networks']           = 'Liste des boutons de vos réseaux sociaux préférés, séparés par un espace et dans l\'ordre d\'affichage. Noms acceptés : twitter facebook googleplus linkedin pinterest tumblr reddit taringa stumbleupon telegram hackernews xing vk email';
 $lang['display']            = 'Options d\'affichage : Montrer le bouton avec l\'icône et le nom, uniquement l\'icône ou le nom sur un fond coloré.';
 $lang['target']             = 'Link target: specifies where to open the social network page.';
 $lang['twitter_user']       = 'Nom d\'utilisateur du compte Twitter de l\'auteur du contenu (ne pas ajouter "@").';
