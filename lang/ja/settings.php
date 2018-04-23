501384e41555892eadce46c3ff18184aba1da78b
Gerrit Uitslag <klapinklapin@gmail.com>
2018-04-23 22:48:00 +0200

    Update settings.php

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -5,11 +5,7 @@
  * @author Makito Katsube <katsubemakito@gmail.com>
  */
 
-// keys need to match the config setting name
-// $lang['fixme'] = 'FIXME';
-
-$lang['networks']           = '表示するボタンを半角スペースで区切り順番に指定します。 指定可能なボタン名は ';
-$lang['networks']          .= 'twitter facebook googleplus linkedin pinterest tumblr reddit taringa stumbleupon telegram hackernews xing vk email';
+$lang['networks']           = '表示するボタンを半角スペースで区切り順番に指定します。 指定可能なボタン名は twitter facebook googleplus linkedin pinterest tumblr reddit taringa stumbleupon telegram hackernews xing vk email';
 $lang['display']            = '表示プション: アイコンと名前の両方、アイコンのみ、背景色の上に名前のみから選択';
 $lang['target']             = 'Link target: specifies where to open the social network page.';
 $lang['twitter_user']       = 'Twitterユーザー名("@"は含めない)';
